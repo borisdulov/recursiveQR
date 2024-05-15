bce2216af1a8d3cc1f4ebfe11ddffb33283c1b7f
Boris Dulov <borisdulov@MacBook-Air-Boris.local>
2024-05-15 14:37:28 +0500

    colors

--- main.py
+++ main.py
@@ -60,6 +60,9 @@ def main():
 
     # Множитель разрешения изображений
     resolution = st.slider("Image scale", min_value=1, max_value=10, value=1, step=1)
+
+    white_color = st.color_picker('Pick a color', '#ffffff')
+    black_color = st.color_picker('Pick a color', '#000000')
     
     if st.button("Generate"):
         # Генерируем внутренний QR
@@ -70,9 +73,20 @@ def main():
 
         # Комбинируем внешний и внутренний коды в одну картинку
         recursive = recurse_qr(inner, outer, resolution)
+        pixels = recursive.getdata()
+
+        # Заменяем белые пиксели на выбранный цвет
+        rgb_color = tuple(int(white_color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+        new_pixels = [rgb_color if pixel == (255, 255, 255) else pixel for pixel in pixels]
+        recursive.putdata(new_pixels)
+
+        # Заменяем черные пиксели на выбранный цвет
+        rgb_color = tuple(int(black_color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+        new_pixels = [rgb_color if pixel == (0, 0, 0) else pixel for pixel in pixels]
+        recursive.putdata(new_pixels)
 
         # Отображаем
-        st.markdown("# Outcome")
+        st.markdown("Result")
         st.image(recursive, use_column_width=True)
         col1, col2 = st.columns(2)
         with col1:
