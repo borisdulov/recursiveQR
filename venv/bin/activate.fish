28ac6c4d2d69ce007604fc2f56ee9b720e41398d
Boris Dulov <borisdulov@MacBook-Air-Boris.local>
2024-05-16 14:29:32 +0500

    nothing really

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/borisdulov/Documents/Projects/Web/gptcalculator/venv"
+set -gx VIRTUAL_ENV "/Users/borisdulov/Documents/Projects/Web/recursiveQRgenerator/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
